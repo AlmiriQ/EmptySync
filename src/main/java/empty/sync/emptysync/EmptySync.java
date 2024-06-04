3f2b804fb568f4b3228a2501535070de1059a5d0
Isaac Iwasaki <50090571+AlmiriQ@users.noreply.github.com>
2024-06-05 06:06:30 +0700

    Add files via upload

--- src/main/java/empty/sync/emptysync/EmptySync.java
+++ src/main/java/empty/sync/emptysync/EmptySync.java
@@ -1,1 +1,79 @@
+package empty.sync.emptysync;
 
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class EmptySync implements DedicatedServerModInitializer {
+    public static final String MOD_ID = "empty-sync";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private final static class Configuration {
+        String host = "localhost";
+        int port = 2222;
+    }
+
+    @Override
+    public void onInitializeServer() {
+        LOGGER.info("Hello Fabric world!");
+        Path configPath = Paths.get("empty-sync.json");
+        if (!configPath.toFile().exists()) {
+            LOGGER.error("Config file (empty-sync.json) not found!");
+            return;
+        }
+
+        Configuration config;
+        {
+            Gson parser = new Gson();
+            try {
+                config = parser.fromJson(new String(Files.readAllBytes(configPath)), Configuration.class);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        HttpClient client = HttpClient.newHttpClient();
+        String defaultURL = "http://" + config.host + ":" + config.port + "/";
+
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(defaultURL + handler.player.getUuid().toString()))
+                    .GET()
+                    .build();
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                String serialized = response.body();
+                if (!serialized.equals("NX"))
+                    SyncableInventory.deserialize(response.body(), handler.player.getInventory());
+            } catch (Exception e) {
+                LOGGER.error("Exception while JOIN: {}", e.toString());
+            }
+        });
+
+        ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+            try {
+                SyncableInventory inventory = new SyncableInventory(handler.player.getInventory());
+                String serialized = inventory.serialize();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(defaultURL + handler.player.getUuid().toString()))
+                        .POST(HttpRequest.BodyPublishers.ofString(serialized))
+                        .build();
+                client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenAccept(response -> {
+                    LOGGER.info("Response: {}", response.body());
+                });
+            } catch (Exception e) {
+                LOGGER.error("Exception while DISCONNECT: {}", e.toString());
+            }
+        });
+
+    }
+}
---
+++ src/main/java/empty/sync/emptysync/NBTConverter.java
@@ -0,0 +1,105 @@
+// https://github.com/KSashaDF/JSON-to-NBT-Converter/blob/master/converter/JsonToNbt.java
+// I hope it's ok to include it here in my package
+package empty.sync.emptysync;
+// author: https://github.com/KSashaDF
+import com.google.gson.*;
+import java.util.Map;
+import java.util.Objects;
+
+import net.minecraft.nbt.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// thank you!
+
+final class NBTConverter {
+    public static final String MOD_ID = "empty-sync-cv";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static NbtElement toNbt(JsonElement jsonElement) {
+        if (jsonElement instanceof JsonPrimitive jsonPrimitive) {
+            LOGGER.warn("JSON element b: {}", jsonPrimitive.isBoolean());
+            LOGGER.warn("JSON element n: {}", jsonPrimitive.isNumber());
+            LOGGER.warn("JSON element s: {}", jsonPrimitive.isString());
+            if (jsonPrimitive.isBoolean()) {
+                boolean value = jsonPrimitive.getAsBoolean();
+                if (value)
+                    return NbtByte.of(true);
+                else
+                    return NbtByte.of(false);
+            } else if (jsonPrimitive.isNumber()) {
+                Number number = jsonPrimitive.getAsNumber();
+                if (number instanceof Byte)
+                    return NbtByte.of(number.byteValue());
+                else if (number instanceof Short)
+                    return NbtShort.of(number.shortValue());
+                else if (number instanceof Integer)
+                    return NbtInt.of(number.intValue());
+                else if (number instanceof Long)
+                    return NbtLong.of(number.longValue());
+                else if (number instanceof Float)
+                    return NbtFloat.of(number.floatValue());
+                else if (number instanceof Double)
+                    return NbtDouble.of(number.doubleValue());
+                else
+                    return NbtDouble.of(number.doubleValue());
+
+            } else if (jsonPrimitive.isString())
+                return NbtString.of(jsonPrimitive.getAsString());
+        } else if (jsonElement instanceof JsonArray jsonArray) {
+            NbtList nbtList = new NbtList();
+            for (JsonElement element : jsonArray)
+                nbtList.add(toNbt(element));
+            return nbtList;
+        } else if (jsonElement instanceof JsonObject jsonObject) {
+            NbtCompound nbtCompound = new NbtCompound();
+            for (Map.Entry<String, JsonElement> jsonEntry : jsonObject.entrySet())
+                nbtCompound.put(jsonEntry.getKey(), toNbt(jsonEntry.getValue()));
+            return nbtCompound;
+        } else if (jsonElement instanceof JsonNull) {
+            return new NbtCompound();
+        }
+        LOGGER.warn("JSON element: {}", jsonElement);
+        throw new AssertionError();
+    }
+
+    public static NbtCompound toNbtCompound(JsonObject jsonObject) {
+        NbtCompound nbtCompound = new NbtCompound();
+        for (Map.Entry<String, JsonElement> jsonEntry : jsonObject.entrySet())
+            nbtCompound.put(jsonEntry.getKey(), toNbt(jsonEntry.getValue()));
+        return nbtCompound;
+    }
+
+    public static JsonElement toJson(NbtElement nbtElement) {
+        switch (nbtElement.getType()) {
+            case NbtElement.BYTE_TYPE:
+                return new JsonPrimitive(((NbtByte) nbtElement).byteValue());
+            case NbtElement.SHORT_TYPE:
+                return new JsonPrimitive(((NbtShort) nbtElement).shortValue());
+            case NbtElement.INT_TYPE:
+                return new JsonPrimitive(((NbtInt) nbtElement).intValue());
+            case NbtElement.LONG_TYPE:
+                return new JsonPrimitive(((NbtLong) nbtElement).longValue());
+            case NbtElement.FLOAT_TYPE:
+                return new JsonPrimitive(((NbtFloat) nbtElement).floatValue());
+            case NbtElement.DOUBLE_TYPE:
+                return new JsonPrimitive(((NbtDouble) nbtElement).doubleValue());
+            case NbtElement.STRING_TYPE:
+                return new JsonPrimitive(nbtElement.asString());
+            case NbtElement.LIST_TYPE:
+                NbtList nbtList = (NbtList) nbtElement;
+                JsonArray jsonArray = new JsonArray();
+                for (NbtElement element : nbtList)
+                    jsonArray.add(toJson(element));
+               return jsonArray;
+            case NbtElement.COMPOUND_TYPE:
+                NbtCompound nbtCompound = (NbtCompound) nbtElement;
+                JsonObject jsonObject = new JsonObject();
+                for (String key : nbtCompound.getKeys())
+                    jsonObject.add(key, toJson(Objects.requireNonNull(nbtCompound.get(key))));
+                return jsonObject;
+            default:
+                throw new AssertionError();
+        }
+    }
+}
